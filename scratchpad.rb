bbc36fd4beb76f1675f34ea3b81cd024dce4d63b
ishanshrimali <ishanshrimali@chicagobooth.edu>
2021-04-08 22:24:19 +0000

    Completed about_classes

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,32 +1,25 @@
-def score(dice)
-  result = 0
-  
-  #First, looks for triplets, take score, and remove from array
-  possible = [1, 2, 3, 4, 5, 6]
-  possible.each do |item|
-    if dice.count(item) >= 3
-      if item == 1
-        result += 1000
-      else
-        result += (item * 100)
-      end
-      dice.delete_at(dice.index(item))
-      dice.delete_at(dice.index(item))
-      dice.delete_at(dice.index(item))
+  class Dog7
+    attr_reader :name
+
+    def initialize(initial_name)
+      @name = initial_name
+    end
+
+    def get_self
+      self
+    end
+
+    def to_s
+      @name
+    end
+
+    def inspect
+      "<Dog named '#{name}'>"
     end
   end
 
-  #Then, take score of remaining individual numbers
-  dice.each do |item|
-    if item == 1
-      result += 100
-    elsif item == 5
-      result += 50
-    else
-    end
-  end
 
-  puts result
-  return result
-  
-end
+    fido = Dog7.new("Fido")
+
+    fidos_self = fido.get_self
+    puts fidos_self
