ef312be1c7dce0186c022906fb5d42be63703cbf
ishanshrimali <ishanshrimali@chicagobooth.edu>
2021-04-08 06:03:22 +0000

    Completed about_scoring_project

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,36 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  result = 0
+  
+  #First, looks for triplets, take score, and remove from array
+  possible = [1, 2, 3, 4, 5, 6]
+  possible.each do |item|
+    if dice.count(item) >= 3
+      if item == 1
+        result += 1000
+      else
+        result += (item * 100)
+      end
+      dice.delete_at(dice.index(item))
+      dice.delete_at(dice.index(item))
+      dice.delete_at(dice.index(item))
+    end
+  end
+
+  #Then, take score of remaining individual numbers
+  dice.each do |item|
+    if item == 1
+      result += 100
+    elsif item == 5
+      result += 50
+    else
+    end
+  end
+
+  puts result
+  return result
+  
 end
 
 class AboutScoringProject < Neo::Koan
